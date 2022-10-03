65c5ef05668cdff28c7d938d2e02e987fcc4d2fb
Dasha <eregina.darya@yandex.ru>
2022-10-03 20:55:25 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 200, 300, 500};
+        SalesManager salesManager = new SalesManager(arr);
+        int max = SalesManager.max();
+        System.out.println(max);
+    }
+}
